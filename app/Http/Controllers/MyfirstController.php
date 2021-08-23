735e55bd0db54a695b0c69d26f68598f7210e7f5
FIRST_NAME LAST_NAME <analizaamparo4@gmail.com>
2021-08-23 19:57:50 +0800

    MyfirstController then add function index

--- app/Http/Controllers/MyfirstController.php
+++ app/Http/Controllers/MyfirstController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class MyfirstController extends Controller
 {
-    //
+    public function index()
+    {
+    	
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/addition', function () {
-    return 1 + 2;
-});
+Route::get('/displaythesum', 'MyfirstController@index);
